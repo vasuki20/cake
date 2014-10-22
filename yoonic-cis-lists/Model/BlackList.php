97d484dc4342546c5bcaa9f72858e46c0ddb4014
unknown <s.vasuki20@gmail.com>
2014-10-23 00:22:39 +0800

    ded model blaclisy

--- yoonic-cis-lists/Model/BlackList.php
+++ yoonic-cis-lists/Model/BlackList.php
@@ -6,7 +6,7 @@ App::uses('AppModel', 'Model');
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ */ 
 class BlackList extends AppModel {
     public function isValueExists($value){
         $this->log("inside isvalue:".$value, 'debug');
