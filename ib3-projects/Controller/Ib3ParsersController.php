5e7055ef14930f6b5cda9d2b4aabfa1185a60202
misite <misite88@gmail.com>
2014-11-05 17:51:37 +0800

    modified Ib3 Controller.php

--- ib3-projects/Controller/Ib3ParsersController.php
+++ ib3-projects/Controller/Ib3ParsersController.php
@@ -13,8 +13,9 @@ class Ib3ParsersController extends AppController {
 
     public function index() {
 
-        require_once '/Applications/XAMPP/xamppfiles/htdocs/testYouTube/google-api-php-client-master/src/Google/Client.php';
-        require_once '/Applications/XAMPP/xamppfiles/htdocs/testYouTube/google-api-php-client-master/src/Google/Service/YouTube.php';
+        require_once 'C:\xampp\php\google-api-php-client-master\src\Google\Client.php';
+        require_once 'C:\xampp\php\google-api-php-client-master\src\Google\Service\YouTube.php';
+
 
         /*
          * Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
@@ -39,7 +40,9 @@ class Ib3ParsersController extends AppController {
         ));
 
 
-
+        print_r($searchResponse);
+        
+        
         $videos = '';
         $channels = '';
         $playlists = '';
@@ -47,25 +50,51 @@ class Ib3ParsersController extends AppController {
         // Add each result to the appropriate list, and then display the lists of
         // matching videos, channels, and playlists.
         foreach ($searchResponse as $searchResult) {
-            print_r($searchResult['snippet']['channelId']);
-            echo '<br>';
-            print_r($searchResult['snippet']['title']);
-            echo '<br>';
-            print_r($searchResult['snippet']['description']);
-            echo '<br>';
-            print_r($searchResult['snippet']['publishedAt']);
-            echo '<br>';
-            print_r($searchResult['snippet']['channelTitle']);
-            echo '<br>';
-            echo '<br>';
-            echo '<br>';
-            echo '<br>';
+//            print_r($searchResult['kind']);
+//            echo '<br>';
+//            print_r($searchResult['etag']);
+//            echo '<br>';
+//            print_r($searchResult['id']['videoId']);
+//            echo '<br>';
+//            print_r($searchResult['snippet']['default']['url']);
+//            echo '<br>';
+//            print_r($searchResult['snippet']['medium']['url']);
+//            echo '<br>';
+//            print_r($searchResult['snippet']['high']['url']);
+//            echo '<br>';
+//            print_r($searchResult['snippet']['channelId']);
+//            echo '<br>';
+//            print_r($searchResult['snippet']['title']);
+//            echo '<br>';
+//            print_r($searchResult['snippet']['description']);
+//            echo '<br>';
+//            print_r($searchResult['snippet']['publishedAt']);
+//            echo '<br>';
+//            print_r($searchResult['snippet']['channelTitle']);
+//            echo '<br>';
+//            echo '<br>';
+//            echo '<br>';
+//            echo '<br>';
+//            echo '<br>';
+//            echo '<br>';
+//            echo '<br>';
+//            echo '<br>';
+//            echo '<br>';
+//            echo '<br>';
+//            echo '<br>';
+
             $this->Searchvideo->Create();
             $insert_data = array("Searchvideo" => array(
+                    "kind" => $searchResult['kind'],
+                    "etag" => $searchResult['etag'],
+                    "id_videoId" => $searchResult['id']['videoId'],
                     "channelId" => $searchResult['snippet']['channelId'],
-                    "title" => $searchResult['snippet']['title"'],
+                    "title" => $searchResult['snippet']['title'],
                     "description" => $searchResult['snippet']['description'],
-                    "publishedAt" => $searchResult['snippet']['publishedAt'],
+                    "thumbnails_default" => $searchResult['snippet']['thumbnails']['default']['url'],
+                    "thumbnails_medium" => $searchResult['snippet']['thumbnails']['medium']['url'],
+                    "thumbnails_high" => $searchResult['snippet']['thumbnails']['high']['url'],
+                    //  "publishedAt" => $searchResult['snippet']['publishedAt'],
                     "channelTitle" => $searchResult['snippet']['channelTitle']
                 )
             );
